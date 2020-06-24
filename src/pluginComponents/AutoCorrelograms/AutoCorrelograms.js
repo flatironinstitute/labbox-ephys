131d524486b17674ca3f04c09802dc11cd97e9df
Jeremy Magland <jmagland@flatironinstitute.org>
2020-06-24 13:58:52 +0000

    add example sorting view and separate out sampleSortingViewProps

--- src/pluginComponents/AutoCorrelograms/AutoCorrelograms.js
+++ src/pluginComponents/AutoCorrelograms/AutoCorrelograms.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MatplotlibPlot from '../../components/MatplotlibPlot';
 import { Grid } from '@material-ui/core';
 import plotStyles from '../common/plotStyles';
+import sampleSortingViewProps from '../common/sampleSortingViewProps'
 
 const AutoCorrelograms = ({ sorting, isSelected, isFocused, onUnitClicked }) => {
     return (
@@ -46,65 +47,7 @@ AutoCorrelograms.sortingViewPlugin = {
 
 AutoCorrelograms.prototypeViewPlugin = {
     label: label,
-    props: {
-        sorting: {
-            "sortingId": "t3BFJYOzea",
-            "sortingLabel": "synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-            "sortingPath": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-            "sortingObject": {
-                "data": {
-                    "firings": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-                    "samplerate": 30000
-                },
-                "sorting_format": "mda"
-            },
-            "recordingId": "6ggv3LEBUm",
-            "recordingPath": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth",
-            "recordingObject": {
-                "data": {
-                    "geom": [
-                        [
-                            1,
-                            0
-                        ],
-                        [
-                            2,
-                            0
-                        ],
-                        [
-                            3,
-                            0
-                        ],
-                        [
-                            4,
-                            0
-                        ]
-                    ],
-                    "params": {
-                        "samplerate": 30000,
-                        "spike_sign": -1
-                    },
-                    "raw": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/raw.mda"
-                },
-                "recording_format": "mda"
-            },
-            "sortingInfo": {
-                "samplerate": 30000,
-                "unit_ids": [
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                    10
-                ]
-            }
-        }
-    }
+    props: sampleSortingViewProps()
 }
 
 export default AutoCorrelograms
--- src/pluginComponents/Units/Units.js
+++ src/pluginComponents/Units/Units.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NiceTable from '../../components/NiceTable'
+import sampleSortingViewProps from '../common/sampleSortingViewProps'
 
 const Units = ({ sorting, recording, isSelected, isFocused, onUnitClicked }) => {
     const selectedRowKeys = []; // TODO: define this based on isSelected
@@ -36,65 +37,7 @@ Units.sortingViewPlugin = {
 
 Units.prototypeViewPlugin = {
     label: label,
-    props: {
-        sorting: {
-            "sortingId": "t3BFJYOzea",
-            "sortingLabel": "synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-            "sortingPath": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-            "sortingObject": {
-                "data": {
-                    "firings": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/firings_true.mda",
-                    "samplerate": 30000
-                },
-                "sorting_format": "mda"
-            },
-            "recordingId": "6ggv3LEBUm",
-            "recordingPath": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth",
-            "recordingObject": {
-                "data": {
-                    "geom": [
-                        [
-                            1,
-                            0
-                        ],
-                        [
-                            2,
-                            0
-                        ],
-                        [
-                            3,
-                            0
-                        ],
-                        [
-                            4,
-                            0
-                        ]
-                    ],
-                    "params": {
-                        "samplerate": 30000,
-                        "spike_sign": -1
-                    },
-                    "raw": "sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth/raw.mda"
-                },
-                "recording_format": "mda"
-            },
-            "sortingInfo": {
-                "samplerate": 30000,
-                "unit_ids": [
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                    10
-                ]
-            }
-        }
-    }
+    props: sampleSortingViewProps()
 }
 
 export default Units
