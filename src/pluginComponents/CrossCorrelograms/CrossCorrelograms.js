2403a14de37520e0151cd6647bf97b564bcc36b6
Jeff Soules <jsoules@flatironinstitute.org>
2020-07-08 14:24:27 -0400

    Fix layout issues; frame now works except for actual plots.

--- src/pluginComponents/CrossCorrelograms/CrossCorrelograms.js
+++ src/pluginComponents/CrossCorrelograms/CrossCorrelograms.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { withSize } from 'react-sizeme';
+// import sizeMe from 'react-sizeme';
 import MatplotlibPlot from '../../components/MatplotlibPlot';
 import { Grid } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Box, CircularProgress } from '@material-ui/core'; // this should go away once we're doing actual plots
 import sampleSortingViewProps from '../common/sampleSortingViewProps';
 
-const CrossCorrelograms = ({ sorting, recording, isSelected, isFocused, onUnitClicked }) => {
+const CrossCorrelograms = ({ size, sorting, recording, isSelected, isFocused, onUnitClicked }) => {
+    const plotMargin = 2; // in pixels
     const [chosenPlots, setChosenPlots] = useState([]);
     const myId =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
@@ -13,10 +16,10 @@ const CrossCorrelograms = ({ sorting, recording, isSelected, isFocused, onUnitCl
         setChosenPlots(sorting.sortingInfo.unit_ids.filter((key) => isSelected(key)));
     };
 
-    const computeLayout = (frameId, marginInPx) => {
-        const width = document.getElementById(frameId).offsetWidth;
+    const n = chosenPlots.length || 0;
+
+    const computeLayout = (marginInPx, maxSize = 500) => {
         // we need to fit a square of side length n elements into the wrapper's width.
-        const n = chosenPlots.length;
         if (n < 2) return;
         // note adjacent margins will collapse, and we don't care about vertical length
         // (the user can scroll). So: horizontal space taken is:
@@ -24,11 +27,14 @@ const CrossCorrelograms = ({ sorting, recording, isSelected, isFocused, onUnitCl
         // width = margin*(n+1) + plotWidth * n
         // Solve for plotWidth = (width - margin*(n+1))/n.
         // And we can't have fractional pixels, so round down.
-        const plotWidth = Math.floor((width - marginInPx*(n + 1))/n);
+        const plotWidth = Math.min(maxSize, Math.floor((size.width - marginInPx*(n + 1))/n));
 
+        alert(`Computed side width is ${plotWidth} for ${n} elements and ${size.width} total space`);
         return plotWidth;
     }
 
+    // pairs are objects of the form '{ xkey: unitId, ykey: unitId }'
+    // This function should return a list of the pairs, in row-major order.
     const makePairs = () => {
         return chosenPlots.reduce((list, xItem) => {
             return list.concat(chosenPlots.map((yItem) => {
@@ -37,29 +43,41 @@ const CrossCorrelograms = ({ sorting, recording, isSelected, isFocused, onUnitCl
         }, []);
     }
 
+    const pairs = makePairs();
+    const plotWidth = computeLayout(plotMargin);
+    const rowBounds = [...Array(pairs.length).keys()].filter(i => i % n === 0);
     return (
         <div style={{'width': '100%'}} id={myId}>
             <Grid container>
                 {
-                    sorting.sortingInfo.unit_ids.map(unitId => (
-                        <Grid key={unitId} item>
-                            <div
-                            >
-                                <div style={{ 'textAlign': 'center' }}>
-                                    <div></div>
-                                </div>
-                                <Box display="flex" width={computeLayout(myId, 1)}
-                                >
-                                    <CircularProgress />
-                                </Box>
-                            </div>
-                        </Grid>
-                    ))
+                    rowBounds.map((start) => renderRow(pairs.slice(start, start + n), plotWidth))
                 }
             </Grid>
             <Button onClick={() => handleUpdateChosenPlots()}>Update selections</Button>
-            <Button onClick={() => {alert(`Pairs: ${JSON.stringify(makePairs())}`)}}>Show Layout</Button>
         </div>
+    );
+}
+
+const renderRow = ( pairs, plotWidth ) => {
+    return (
+        <Grid>
+            {
+                pairs.map((pair) => (
+                    <Grid key={pair.xkey + '-' + pair.ykey} item>
+                        <div
+                        >
+                            <div style={{ 'textAlign': 'center' }}>
+                                <div>{pair.xkey + ' vs ' + pair.ykey}</div>
+                            </div>
+                            <Box display="flex" width={plotWidth + "px"} height={plotWidth + 'px'}
+                            >
+                                <CircularProgress />
+                            </Box>
+                        </div>
+                    </Grid>
+                ))
+            }
+        </Grid>
     );
 }
 
@@ -74,4 +92,14 @@ CrossCorrelograms.prototypeViewPlugin = {
     props: sampleSortingViewProps()
 }
 
-export default CrossCorrelograms
+// export default CrossCorrelograms;
+const exportedComponent = withSize()(CrossCorrelograms);
+exportedComponent.sortingViewPlugin = {
+    label: label
+}
+exportedComponent.prototypeViewPlugin = {
+    label: label,
+    props: sampleSortingViewProps()
+}
+
+export default exportedComponent;
