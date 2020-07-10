cf70424ed69b032d55796d9e67ac06a8205dddcd
Jeff Soules <jsoules@flatironinstitute.org>
2020-07-10 14:59:09 -0400

    Unify cross-, auto-correlogram data generation function.

--- src/pluginComponents/AutoCorrelograms/genplot_autocorrelogram.py
+++ src/pluginComponents/AutoCorrelograms/genplot_autocorrelogram.py
@@ -9,9 +9,11 @@ def genplot_autocorrelogram(sorting_object, unit_id):
     import labbox_ephys as le
 
     S = le.LabboxEphysSortingExtractor(sorting_object)
+    bins, bin_counts, bin_size = _get_correlogram_data(sorting=S, unit_id1=unit_id,
+        window_size_msec=50, bin_size_msec=1)
 
     f = plt.figure(figsize=(2, 2))
-    _plot_autocorrelogram(ax=plt.gca(), sorting=S, unit_id=unit_id, window_size_msec=50, bin_size_msec=1)
+    _plot_correlogram(ax=plt.gca(), bin_counts=bin_counts, bins=bins, wid=bin_size, color='gray')
     return mpld3.fig_to_dict(f)
 
 @hi.function('genplot_crosscorrelogram', '0.1.0')
@@ -20,35 +22,13 @@ def genplot_crosscorrelogram(sorting_object, x_unit_id, y_unit_id, plot_edge_siz
     import labbox_ephys as le
 
     S = le.LabboxEphysSortingExtractor(sorting_object)
-
     f = plt.figure(figsize=(plot_edge_size, plot_edge_size))
 
-    if x_unit_id != y_unit_id:
-        _plot_crosscorrelogram(ax=plt.gca(), sorting=S, unit_id1=x_unit_id,
-            unit_id2=y_unit_id,window_size_msec=50, bin_size_msec=1)
-    else:
-        _plot_autocorrelogram(ax=plt.gca(), sorting=S, unit_id=x_unit_id,
-            window_size_msec=50, bin_size_msec=1)
+    bins, bin_counts, bin_size = _get_correlogram_data(sorting=S, unit_id1=x_unit_id,
+        unit_id2=y_unit_id, window_size_msec=50, bin_size_msec=1)
+    _plot_correlogram(ax=plt.gca(), bin_counts=bin_counts, bins=bins, wid=bin_size, color='gray')
+
     return mpld3.fig_to_dict(f)
-
-@hi.function('gendata_crosscorrelogram', '0.1.0')
-def gendata_crosscorrelogram(sorting_object, x_unit_id, y_unit_id, plot_edge_size):
-    import labbox_ephys as le
-
-    S = le.LabboxEphysSortingExtractor(sorting_object)
-
-    if x_unit_id != y_unit_id:
-        bins, bin_counts, bin_size_sec = _get_crosscorrelogram_data(
-            sorting=S, unit_id1=x_unit_id,
-            unit_id2=y_unit_id,window_size_msec=50, bin_size_msec=1
-        )
-        return dict(
-            bins=bins,
-            bin_counts=bin_counts,
-            bin_size_sec=bin_size_sec
-        )
-    else:
-        raise Exception('needs to be implemented')
 
 def _plot_correlogram(*, ax, bin_counts, bins, wid, title='', color=None):
     kk = 1000
@@ -58,49 +38,32 @@ def _plot_correlogram(*, ax, bin_counts, bins, wid, title='', color=None):
     ax.set_xticks([bins[0]*kk, bins[len(bins)//2]*kk, bins[-1]*kk])
     # ax.set_yticks([])
 
-
-def _plot_autocorrelogram(ax, sorting, unit_id, window_size_msec, bin_size_msec):
-    times = sorting.get_unit_spike_train(unit_id=unit_id)
+def _get_correlogram_data(*, sorting, unit_id1, unit_id2=None, window_size_msec, bin_size_msec):
+    auto = unit_id2 is None or unit_id2 == unit_id1
+    
+    times = sorting.get_unit_spike_train(unit_id=unit_id1)
+    window_size = window_size_msec / 1000
+    bin_size = bin_size_msec / 1000
     labels = np.ones(times.shape, dtype=np.int32)
-    window_size = window_size_msec / 1000
-    bin_size = bin_size_msec / 1000
+    cluster_ids = [1]
+    if not auto:
+        times2 = sorting.get_unit_spike_train(unit_id=unit_id2)
+        times = np.concatenate((times, times2))
+        labels = np.concatenate((labels, np.ones(times2.shape, dtype=np.int32) *2 ))
+        cluster_ids.append(2)
+        inds = np.argsort(times)
+        times = times[inds]
+        labels = labels[inds]
     C = compute_correlograms(
         spike_times=times/sorting.get_sampling_frequency(),
         spike_clusters=labels,
-        cluster_ids=[1],
+        cluster_ids=cluster_ids,
         bin_size=bin_size,
         window_size=window_size,
         sample_rate=sorting.get_sampling_frequency(),
         symmetrize=True
     )
     bins = np.linspace(- window_size / 2, window_size / 2, C.shape[2])
-    _plot_correlogram(ax=ax, bin_counts=C[0, 0, :], bins=bins, wid=bin_size, color='gray')
-
-def _get_crosscorrelogram_data(sorting, unit_id1, unit_id2, window_size_msec, bin_size_msec):
-    times1 = sorting.get_unit_spike_train(unit_id=unit_id1)
-    times2 = sorting.get_unit_spike_train(unit_id=unit_id2)
-    times = np.concatenate((times1, times2))
-    labels = np.concatenate(
-        (np.ones(times1.shape, dtype=np.int32)*1, np.ones(times2.shape, dtype=np.int32)*2))
-    inds = np.argsort(times)
-    times = times[inds]
-    labels = labels[inds]
-    window_size = window_size_msec / 1000
-    bin_size = bin_size_msec / 1000
-    C = compute_correlograms(
-        spike_times=times/sorting.get_sampling_frequency(),
-        spike_clusters=labels,
-        cluster_ids=[1, 2],
-        bin_size=bin_size,
-        window_size=window_size,
-        sample_rate=sorting.get_sampling_frequency(),
-        symmetrize=True
-    )
-    bins = np.linspace(- window_size / 2, window_size / 2, C.shape[2])
-    bin_counts = C[0, 1, :]
+    bin_counts = C[0, 0, :] if auto else C[0, 1, :]
     bin_size_sec = bin_size_msec / 1000
     return bins, bin_counts, bin_size_sec
-
-def _plot_crosscorrelogram(ax, sorting, unit_id1, unit_id2, window_size_msec, bin_size_msec):
-    bins, bin_counts, bin_size = _get_crosscorrelogram_data(sorting, unit_id1, unit_id2, window_size_msec, bin_size_msec)
-    _plot_correlogram(ax=ax, bin_counts=bin_counts, bins=bins, wid=bin_size, color='gray')
