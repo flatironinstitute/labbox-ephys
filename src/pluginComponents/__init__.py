1080c7eb68e6119125f226cab378136ef64c9670
Jeff Soules <jsoules@flatironinstitute.org>
2020-07-17 15:48:44 -0400

    Adds call to hither function to compute and display firing event counts, rates in units table.

--- src/pluginComponents/__init__.py
+++ src/pluginComponents/__init__.py
@@ -3,3 +3,4 @@ from .BokehTest2.bokeh_test_2 import bokeh_test_2
 from .MatplotlibTest.test_mpl import test_mpl
 
 from .AutoCorrelograms.genplot_autocorrelogram import fetch_correlogram_plot_data
+from .Units.units import get_firing_data, get_structure
