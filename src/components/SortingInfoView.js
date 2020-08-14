89cc0db4f1b7653ad53868f745ff2a203d1dcabf
Jeff Soules <jsoules@flatironinstitute.org>
2020-08-14 10:00:42 -0400

    Trying to fix perfomrance. Failing so far.

--- src/components/SortingInfoView.js
+++ src/components/SortingInfoView.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import SortingInfoUnitEntry from '../pluginComponents/SortingInfoUnitEntry';
+// import useTraceUpdate from 'use-trace-update';
 
-const SortingInfoView = ({ sortingInfo, isSelected, isFocused, onUnitClicked, curation={}, styling }) => {
+const SortingInfoView = ({ sortingInfo, selections = {}, focus = -999, onUnitClicked, curation={}, styling }) => {
+    // useTraceUpdate({ sortingInfo, selections, focus, onUnitClicked, curation, styling });
     const si = sortingInfo;
     if (!si) return <div>No sorting info found</div>;
 
@@ -15,8 +18,6 @@ const SortingInfoView = ({ sortingInfo, isSelected, isFocused, onUnitClicked, cu
         'border': 'solid 1px #4287f5',
         'backgroundColor': '#c0d9ff',
         'color': 'black'
-        // color: '#4287f5',
-        // 'backgroundColor': 'white',
     }
     
     const selectedStyle = {
@@ -54,51 +55,72 @@ const SortingInfoView = ({ sortingInfo, isSelected, isFocused, onUnitClicked, cu
     }
     
     const SortingViewDiv = ({ unit_ids }) => {
+        const unitTuples = unit_ids.reduce((styleList, unitId) => {
+            return {
+                ...styleList,
+                [unitId]: {
+                    'style': unselectedStyle,
+                    'labelStyle': unselectedLabelStyle,
+                    'label': ((curation[unitId] || {}).labels || [])
+                }
+            }
+        }, {});
+        Object.keys(selections).forEach((unitId) => {
+            if (!unitTuples[unitId]) return;
+            unitTuples[unitId]['style'] = selectedStyle;
+            unitTuples[unitId]['labelStyle'] = selectedLabelStyle;
+        });
+        if (focus !== -999 && selections[focus]) {
+            unitTuples[focus]['style'] = focusedStyle;
+            unitTuples[focus]['labelStyle'] = focusedLabelStyle;
+        }
         return (
             <div style={styling} key={unit_ids}>
                 <div style={labelStyle}>Unit IDs</div>
-                {unit_ids.map((unitId, idx, ary) => clickabilize(unitId, idx, ary))}
+                {unit_ids.map((unitId, idx, ary) => (
+                    <SortingInfoUnitEntry
+                        unitId={unitId}
+                        labels={unitTuples[unitId]['labels']}
+                        unitIdStyle={unitIdStyle}
+                        unitStyle={unitTuples[unitId]['style']}
+                        labelStyle={unitTuples[unitId]['labelStyle']}
+                        onUnitClicked={onUnitClicked}
+                    />
+                ))}
             </div>
         )
-    }
-
-    const GetUnitStyle = (unitId) => {
-        return ((isSelected && isSelected(unitId))
-                ? (isFocused && isFocused(unitId)
-                    ? focusedStyle
-                    : selectedStyle)
-                : unselectedStyle);
-    }
-
-    const GetLabelsStyle = (unitId) => {
-        return ((isSelected && isSelected(unitId))
-                ? ((isFocused && isFocused(unitId))
-                    ? focusedLabelStyle
-                    : selectedLabelStyle)
-                : unselectedLabelStyle);
     }
         
     function clickabilize(unitId, idx, ary) {
         return (
-            <div
-                key={unitId}
-                style={GetUnitStyle(unitId)}
-                onClick={(event) => onUnitClicked(unitId, event)}
-            >
-                <span style={unitIdStyle} key={unitId + '-name'}>{unitId}</span>
-                <span key={unitId + '-curationLabels'}>
-                    {((curation[unitId] || {}).labels || [])
-                        .map((label) => {
-                            return (
-                                <span style={GetLabelsStyle(unitId)}
-                                    key={unitId + '-label-' + label}
-                                >
-                                    {label}&nbsp;
-                                </span>
-                            )
-                        })}
-                </span>
-            </div>
+            <SortingInfoUnitEntry
+                unitId={unitId}
+                labels = {(curation[unitId] || {}).labels || []}
+                unitIdStyle={unitIdStyle}
+                // unitStyle={GetUnitStyle(unitId)}
+                // labelStyle={GetLabelsStyle(unitId)}
+                onUnitClicked={onUnitClicked}
+            />
+            // <div
+            //     key={unitId}
+            //     style={GetUnitStyle(unitId)}
+            //     onClick={(event) => onUnitClicked(unitId, event)}
+            // >
+            //     <span style={unitIdStyle} key={unitId + '-name'}>{unitId}</span>
+            //     <span key={unitId + '-curationLabels'}>
+            //         {((curation[unitId] || {}).labels || [])
+            //             .map((label) => {
+            //                 return (
+            //                     <span style={GetLabelsStyle(unitId)}
+            //                         key={unitId + '-label-' + label}
+            //                     >
+            //                         {label}&nbsp;
+            //                     </span>
+            //                 )
+            //             })}
+            //     </span>
+            // </div>
+
         );
     }
 
---
+++ src/pluginComponents/common/unitViewStyles.js
@@ -0,0 +1,44 @@
+const unitViewStyles = {
+    'labelStyle': {
+        'minWidth': '200px',
+        'fontWeight': 'bold',
+        'padding': '7px 5px 7px 5px'
+    },
+    'focusedStyle': {
+        'fontWeight': 'bold',
+        'border': 'solid 1px #4287f5',
+        'backgroundColor': '#c0d9ff',
+        'color': 'black'
+    },
+    'selectedStyle': {
+        'border': 'solid 1px blue',
+        'fontWeight': 'bold',
+        'color': 'black',
+        'backgroundColor': '#b5d1ff',
+    },
+    'unselectedStyle': {
+        'border': 'transparent 1px solid',
+        'fontWeight': 'normal',
+        'color': 'black',
+        'backgroundColor': 'white',
+    },
+    'unitIdStyle': {
+        'paddingRight': '13px',
+        'paddingTop': '10px'
+    },
+    'focusedLabelStyle': {
+        'paddingRight': '3px',
+        'color': '#333333'
+    },
+    'selectedLabelStyle': {
+        'paddingRight': '3px',
+        'color': '#333333'
+    },
+    'unselectedLabelStyle': {
+        'paddingRight': '3px',
+        'color': '#333333',
+    }
+}
+
+
+export default unitViewStyles;
