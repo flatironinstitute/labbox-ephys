860b4062fbc58254084c2d19b4c19fdbdb8a233f
Jeremy Magland <jmagland@flatironinstitute.org>
2020-08-18 14:26:50 +0000

    fix rerender issue by un-nesting component definitions

--- src/components/SortingInfoView.js
+++ src/components/SortingInfoView.js
@@ -2,94 +2,94 @@ import React from 'react';
 import SortingInfoUnitEntry from '../pluginComponents/SortingInfoUnitEntry';
 // import useTraceUpdate from 'use-trace-update';
 
+const labelStyle = {
+    'minWidth': '200px',
+    'fontWeight': 'bold',
+    'padding': '7px 5px 7px 5px'
+}
+
+const focusedStyle = {
+    'fontWeight': 'bold',
+    'border': 'solid 1px #4287f5',
+    'backgroundColor': '#c0d9ff',
+    'color': 'black'
+}
+
+const selectedStyle = {
+    'border': 'solid 1px blue',
+    'fontWeight': 'bold',
+    color: 'black',
+    'backgroundColor': '#b5d1ff',
+}
+
+const unselectedStyle = {
+    'border': 'transparent 1px solid',
+    'fontWeight': 'normal',
+    color: 'black',
+    'backgroundColor': 'white',
+}
+
+const unitIdStyle = {
+    'paddingRight': '13px',
+    'paddingTop': '10px'
+}
+
+const focusedLabelStyle = {
+    'paddingRight': '3px',
+    'color': '#333333'
+}
+
+const selectedLabelStyle = {
+    'paddingRight': '3px',
+    'color': '#333333'
+}
+
+const unselectedLabelStyle = {
+    'paddingRight': '3px',
+    'color': '#333333',
+}
+
+const SortingViewDiv = ({ unit_ids, onUnitClicked, curation, focus, styling, selections }) => {
+    const unitTuples = unit_ids.reduce((styleList, unitId) => {
+        return {
+            ...styleList,
+            [unitId]: {
+                'style': unselectedStyle,
+                'labelStyle': unselectedLabelStyle,
+                'label': ((curation[unitId] || {}).labels || [])
+            }
+        }
+    }, {});
+    Object.keys(selections).forEach((unitId) => {
+        if (!unitTuples[unitId]) return;
+        unitTuples[unitId]['style'] = selectedStyle;
+        unitTuples[unitId]['labelStyle'] = selectedLabelStyle;
+    });
+    if (focus !== -999 && selections[focus]) {
+        unitTuples[focus]['style'] = focusedStyle;
+        unitTuples[focus]['labelStyle'] = focusedLabelStyle;
+    }
+    return (
+        <div style={styling} key={unit_ids}>
+            <div style={labelStyle}>Unit IDs</div>
+            {unit_ids.map((unitId, idx, ary) => (
+                <SortingInfoUnitEntry
+                    unitId={unitId}
+                    labels={unitTuples[unitId]['labels']}
+                    unitIdStyle={unitIdStyle}
+                    unitStyle={unitTuples[unitId]['style']}
+                    labelStyle={unitTuples[unitId]['labelStyle']}
+                    onUnitClicked={onUnitClicked}
+                />
+            ))}
+        </div>
+    )
+}
+
 const SortingInfoView = ({ sortingInfo, selections = {}, focus = -999, onUnitClicked, curation={}, styling }) => {
     // useTraceUpdate({ sortingInfo, selections, focus, onUnitClicked, curation, styling });
     const si = sortingInfo;
     if (!si) return <div>No sorting info found</div>;
-
-    const labelStyle = {
-        'minWidth': '200px',
-        'fontWeight': 'bold',
-        'padding': '7px 5px 7px 5px'
-    }
-    
-    const focusedStyle = {
-        'fontWeight': 'bold',
-        'border': 'solid 1px #4287f5',
-        'backgroundColor': '#c0d9ff',
-        'color': 'black'
-    }
-    
-    const selectedStyle = {
-        'border': 'solid 1px blue',
-        'fontWeight': 'bold',
-        color: 'black',
-        'backgroundColor': '#b5d1ff',
-    }
-    
-    const unselectedStyle = {
-        'border': 'transparent 1px solid',
-        'fontWeight': 'normal',
-        color: 'black',
-        'backgroundColor': 'white',
-    }
-
-    const unitIdStyle = {
-        'paddingRight': '13px',
-        'paddingTop': '10px'
-    }
-
-    const focusedLabelStyle = {
-        'paddingRight': '3px',
-        'color': '#333333'
-    }
-
-    const selectedLabelStyle = {
-        'paddingRight': '3px',
-        'color': '#333333'
-    }
-
-    const unselectedLabelStyle = {
-        'paddingRight': '3px',
-        'color': '#333333',
-    }
-    
-    const SortingViewDiv = ({ unit_ids }) => {
-        const unitTuples = unit_ids.reduce((styleList, unitId) => {
-            return {
-                ...styleList,
-                [unitId]: {
-                    'style': unselectedStyle,
-                    'labelStyle': unselectedLabelStyle,
-                    'label': ((curation[unitId] || {}).labels || [])
-                }
-            }
-        }, {});
-        Object.keys(selections).forEach((unitId) => {
-            if (!unitTuples[unitId]) return;
-            unitTuples[unitId]['style'] = selectedStyle;
-            unitTuples[unitId]['labelStyle'] = selectedLabelStyle;
-        });
-        if (focus !== -999 && selections[focus]) {
-            unitTuples[focus]['style'] = focusedStyle;
-            unitTuples[focus]['labelStyle'] = focusedLabelStyle;
-        }
-        return (
-            <div style={styling} key={unit_ids}>
-                <div style={labelStyle}>Unit IDs</div>
-                {unit_ids.map((unitId, idx, ary) => (
-                    <SortingInfoUnitEntry
-                        unitId={unitId}
-                        labels={unitTuples[unitId]['labels']}
-                        unitIdStyle={unitIdStyle}
-                        unitStyle={unitTuples[unitId]['style']}
-                        labelStyle={unitTuples[unitId]['labelStyle']}
-                        onUnitClicked={onUnitClicked}
-                    />
-                ))}
-            </div>
-        )
-    }
         
     function clickabilize(unitId, idx, ary) {
         return (
@@ -126,7 +126,13 @@ const SortingInfoView = ({ sortingInfo, selections = {}, focus = -999, onUnitCli
 
     return (
         <div>
-            <SortingViewDiv unit_ids={si.unit_ids}
+            <SortingViewDiv
+                unit_ids={si.unit_ids}
+                onUnitClicked={onUnitClicked}
+                curation={curation}
+                focus={focus}
+                styling={styling}
+                selections={selections}
             />
         </div>
     );
