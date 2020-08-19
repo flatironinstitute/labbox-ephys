aeb9f042f6e78372a5ef71d9efdf3de81359edd8
Jeremy Magland <jmagland@flatironinstitute.org>
2020-08-19 18:39:07 +0000

    incr version

--- python/labbox_ephys/__init__.py
+++ python/labbox_ephys/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 
 from .extractors import LabboxEphysRecordingExtractor, LabboxEphysSortingExtractor
 from .extractors import MdaRecordingExtractor, MdaSortingExtractor
