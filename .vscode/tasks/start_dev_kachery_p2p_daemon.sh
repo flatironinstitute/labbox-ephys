e581d0290b979cedc9bb9ca764c25d7ded7c26bd
Jeremy Magland <jmagland@flatironinstitute.org>
2020-08-20 12:45:08 +0000

    change to flatiron1 channel for testing

--- .vscode/tasks/start_dev_kachery_p2p_daemon.sh
+++ .vscode/tasks/start_dev_kachery_p2p_daemon.sh
@@ -4,4 +4,4 @@ set -ex
 
 # TODO: set kachery-p2p environment variables here
 
-kachery-p2p-start-daemon --method dev --verbose 200 --channel spikeforest
+kachery-p2p-start-daemon --method dev --verbose 200 --channel flatiron1
