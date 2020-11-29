f9c96e804eef96ef32ee6b1840ff7af4c9188503
Jeremy Magland <jmagland@flatironinstitute.org>
2020-11-29 03:10:24 +0000

    incr version

--- python/labbox_ephys/__init__.py
+++ python/labbox_ephys/__init__.py
@@ -1,21 +1,16 @@
-__version__ = "0.4.0"
-
-from .extractors import LabboxEphysRecordingExtractor, LabboxEphysSortingExtractor
-from .extractors import MdaRecordingExtractor, MdaSortingExtractor
-from .extractors import writemda32
-
-from .misc import get_recording_object
-from .misc import get_recording_info
-from .misc import get_sorting_info
+__version__ = "0.4.1"
 
 from .extensions import *
-
-from .sorters import sorters
-
-from .helpers.get_unit_waveforms import get_unit_waveforms
+from .extractors import (LabboxEphysRecordingExtractor,
+                         LabboxEphysSortingExtractor, MdaRecordingExtractor,
+                         MdaSortingExtractor, writemda32)
 from .helpers.find_unit_neighborhoods import find_unit_neighborhoods
 from .helpers.find_unit_peak_channels import find_unit_peak_channels
+from .helpers.get_unit_waveforms import get_unit_waveforms
+from .helpers.prepare_snippets_h5 import (prepare_snippets_h5,
+                                          prepare_snippets_h5_from_extractors)
 from .helpers.SubsampledSortingExtractor import SubsampledSortingExtractor
-from .helpers.prepare_snippets_h5 import prepare_snippets_h5, prepare_snippets_h5_from_extractors
+from .misc import get_recording_info, get_recording_object, get_sorting_info
+from .sorters import sorters
 
 dummy = 0
