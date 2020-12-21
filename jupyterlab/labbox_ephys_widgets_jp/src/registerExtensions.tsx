dcf7823d8ef5e68d8b3f11366ea0c857bbb8749d
Jeremy Magland <jmagland@flatironinstitute.org>
2020-12-21 18:05:37 -0500

    fix code generation

--- jupyterlab/labbox_ephys_widgets_jp/src/registerExtensions.tsx
+++ jupyterlab/labbox_ephys_widgets_jp/src/registerExtensions.tsx
@@ -2,17 +2,17 @@
 // !begin-code-generation!
 import { ExtensionContext } from './extensions/extensionInterface';
 
-import { activate as activatecorrelograms } from './extensions/correlograms/correlograms';
-import { activate as activateexample } from './extensions/example/example';
-import { activate as activatespikeamplitudes } from './extensions/spikeamplitudes/spikeamplitudes';
-import { activate as activatedevel } from './extensions/devel/devel';
-import { activate as activateelectrodegeometry } from './extensions/electrodegeometry/electrodegeometry';
-import { activate as activatetimeseries } from './extensions/timeseries/timeseries';
-import { activate as activateaveragewaveforms } from './extensions/averagewaveforms/averagewaveforms';
-import { activate as activateunitstable } from './extensions/unitstable/unitstable';
-import { activate as activatemountainview } from './extensions/mountainview/mountainview';
-import { activate as activatecuration } from './extensions/curation/curation';
-import { activate as activateaveragewaveformsold } from './extensions/averagewaveformsold/averagewaveformsold';
+import { activate as activatecorrelograms } from './extensions/correlograms/correlograms'
+import { activate as activateexample } from './extensions/example/example'
+import { activate as activatespikeamplitudes } from './extensions/spikeamplitudes/spikeamplitudes'
+import { activate as activatedevel } from './extensions/devel/devel'
+import { activate as activateelectrodegeometry } from './extensions/electrodegeometry/electrodegeometry'
+import { activate as activatetimeseries } from './extensions/timeseries/timeseries'
+import { activate as activateaveragewaveforms } from './extensions/averagewaveforms/averagewaveforms'
+import { activate as activateunitstable } from './extensions/unitstable/unitstable'
+import { activate as activatemountainview } from './extensions/mountainview/mountainview'
+import { activate as activatecuration } from './extensions/old/curation/curation'
+import { activate as activateaveragewaveformsold } from './extensions/old/averagewaveformsold/averagewaveformsold'
 // !end-code-generation!
 
 const registerExtensions = (context: ExtensionContext) => {
