bcab8831dd8790b608affcf3a6bea1f6eaf3b8dc
Jeremy Magland <jmagland@flatironinstitute.org>
2021-01-04 15:03:32 -0500

    fix jupyterlab hither imports

--- src/extensions/common/hither/index.ts
+++ src/extensions/common/hither/index.ts
@@ -1,9 +1,9 @@
 import createCalculationPool, { CalculationPool } from './createCalculationPool'
 import HitherContext from './HitherContext'
-import { HitherInterface, HitherJob } from './HitherInterface'
+import { HitherInterface, HitherJob, HitherJobOpts } from './HitherInterface'
 import HitherJobStatusView from './HitherJobStatusView'
 import useHitherJob from './useHitherJob'
 
-export type { CalculationPool, HitherInterface, HitherJob }
+export type { CalculationPool, HitherInterface, HitherJob, HitherJobOpts }
 export { HitherContext, HitherJobStatusView, useHitherJob, createCalculationPool }
 
