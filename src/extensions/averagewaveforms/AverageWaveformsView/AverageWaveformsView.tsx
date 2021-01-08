c04540a95f3a87d5b1568f194d8c12eab9fe623d
Jeff Soules <jsoules@flatironinstitute.org>
2021-01-08 16:36:14 -0500

    Rebase from updated master.

--- src/extensions/averagewaveforms/AverageWaveformsView/AverageWaveformsView.tsx
+++ src/extensions/averagewaveforms/AverageWaveformsView/AverageWaveformsView.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent, useMemo } from 'react';
+import sizeMe, { SizeMeProps } from 'react-sizeme';
 import SortingUnitPlotGrid from '../../common/SortingUnitPlotGrid';
+import Splitter from '../../common/Splitter';
 import { SortingViewProps } from "../../extensionInterface";
 import AverageWaveformView2 from './AverageWaveformView2';
 
-const AverageWaveformsView: FunctionComponent<SortingViewProps> = (props) => {
+const AverageWaveformsView: FunctionComponent<SortingViewProps & SizeMeProps> = (props) => {
     const {recording, sorting, selection, selectionDispatch} = props
     const boxHeight = 250
     const boxWidth = 180
-
     const noiseLevel = (recording.recordingInfo || {}).noise_level || 1  // fix this
     const unitComponent = useMemo(() => (unitId: number) => (
         <AverageWaveformView2
@@ -18,17 +19,33 @@ const AverageWaveformsView: FunctionComponent<SortingViewProps> = (props) => {
         />
     ), [sorting, recording, selection, selectionDispatch, noiseLevel])
 
+    const width = props.size.width;
+    const height = 650 // hard-coded as per TimeseriesForRecordingView.tsx
 
-    // useCheckForChanges('AverageWaveformsView', props)
+    if (!width) return <div>No width</div>
 
     return (
-        <SortingUnitPlotGrid
-            sorting={sorting}
-            selection={selection}
-            selectionDispatch={selectionDispatch}
-            unitComponent={unitComponent}
-        />
+        <div>
+            <Splitter
+                width={width}
+                height={height}
+                initialPosition={400}
+            >
+                {
+                    <div>Hello World!</div>
+                }
+                {
+                    <SortingUnitPlotGrid
+                        sorting={sorting}
+                        selection={selection}
+                        selectionDispatch={selectionDispatch}
+                        unitComponent={unitComponent}
+                    />
+                }
+
+            </Splitter>
+        </div>
     );
 }
 
-export default AverageWaveformsView
+export default sizeMe()(AverageWaveformsView)
