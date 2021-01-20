4dec26c54a875bf2c5708d8c4c59f7dce4724ab0
Jeremy Magland <jeremy.magland@gmail.com>
2021-01-19 20:27:40 -0500

    remove unused code

--- src/reducers/createHitherJob.ts
+++ src/reducers/createHitherJob.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CalculationPool, createCalculationPool, HitherJob } from '../extensions/common/hither';
+import { CalculationPool, HitherJob } from '../extensions/common/hither';
 
 const objectHash = require('object-hash');
 
@@ -227,108 +227,9 @@ class ClientHitherJob {
   }
 }
 
-const processHitherJobResult = (x: any): any => {
-  if (typeof(x) === 'object') {
-    if (Array.isArray(x)) {
-      return x.map(a => processHitherJobResult(a))
-    }
-    else if (x._type === 'ndarray') {
-      const shape = x.shape as number[]
-      const dtype = x.dtype as string
-      const data_b64 = x.data_b64 as string
-      const dataBuffer = _base64ToArrayBuffer(data_b64)
-      if (dtype === 'float32') { 
-        return applyShape(new Float32Array(dataBuffer), shape)
-      }
-      else if (dtype === 'int32') { 
-        return applyShape(new Int32Array(dataBuffer), shape)
-      }
-      else if (dtype === 'int16') {
-        return applyShape(new Int16Array(dataBuffer), shape)
-      }
-      else {
-        throw Error(`Datatype not yet implemented for ndarray: ${dtype}`)
-      }
-    }
-    else {
-      const ret: {[key: string]: any} = {}
-      for (let k in x) {
-        ret[k] = processHitherJobResult(x[k])
-      }
-      return ret
-    }
-  }
-  else return x
-}
-
-const applyShape = (x: Float32Array | Int32Array | Int16Array, shape: number[]): number[] | number[][] => {
-  if (shape.length === 1) {
-    if (shape[0] !== x.length) throw Error('Unexpected length of array')
-    return Array.from(x)
-  }
-  else if (shape.length === 2) {
-    const n1 = shape[0]
-    const n2 = shape[1]
-    if (n1 * n2 !== x.length) throw Error('Unexpected length of array')
-    const ret: number[][] = []
-    for (let i1 = 0; i1 < n1; i1++) {
-      ret.push(Array.from(x.slice(i1 * n2, (i1 + 1) * n2)))
-    }
-    return ret
-  }
-  else {
-    throw Error('Not yet implemented')
-  }
-}
-
-const _base64ToArrayBuffer = (base64: string): ArrayBuffer => {
-	var binary_string = window.atob(base64)
-	var len = binary_string.length
-	var bytes = new Uint8Array(len)
-	for (var i = 0; i < len; i++) {
-		bytes[i] = binary_string.charCodeAt(i)
-	}
-	return bytes.buffer
-}
-
 interface HitherJobOpts {
   useClientCache?: boolean
   calculationPool?: CalculationPool
-}
-
-const defaultCalculationPool = createCalculationPool({maxSimultaneous: 20})
-
-const createHitherJob = (functionName: string, kwargs: {[key: string]: any}, opts: HitherJobOpts): HitherJob => {
-  const jobHash = objectHash({ functionName, kwargs, opts: {useClientCache: opts.useClientCache} }); // important: we exclude calculationPool in this hash
-  if (opts.useClientCache) {
-    const existingJob = globalData.hitherClientJobCache[jobHash];
-    if (existingJob) return existingJob.object()
-  }
-  const J = new ClientHitherJob({functionName, kwargs, opts});
-  globalData.hitherJobs[J._object.clientJobId] = J;
-
-  if (opts.useClientCache) {
-    globalData.hitherClientJobCache[jobHash] = J;
-  }
-  const apiConnection = globalData.apiConnection
-  if (!apiConnection) {
-    throw Error('Cannot create hither job with no API connection')
-  }
-
-  (opts.calculationPool || defaultCalculationPool).requestSlot().then(({complete}) => {
-    J.onError(() => {complete()})
-    J.onFinished(() => {
-      complete()
-    })
-    apiConnection.sendMessage({
-      type: 'hitherCreateJob',
-      functionName: J._object.functionName,
-      kwargs: J._object.kwargs,
-      clientJobId: J._object.clientJobId
-    })
-  })
-
-  return J.object()
 }
 
 function randomString(num_chars: number) {
