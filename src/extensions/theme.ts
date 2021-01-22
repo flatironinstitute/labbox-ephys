66eacdf7d76edfab3cb56779e9173f46cfa82d81
Jeremy Magland <jeremy.magland@gmail.com>
2021-01-22 17:44:24 -0500

    jupyter curation feeds

--- src/extensions/theme.ts
+++ src/extensions/theme.ts
@@ -9,37 +9,6 @@ const themeOptions: ThemeOptions = {
         secondary: indigo,
     },
     overrides: {
-        MuiAccordionSummary: {
-            root: {
-                '&$expanded': {
-                    minHeight: 0,
-                    maxHeight: 30,
-                    paddingTop: 10
-                },
-                minHeight: 0
-            }
-        },
-        MuiToolbar: {
-            regular: {
-                minHeight: 48,
-                '@media(min-width:600px)' : {
-                    minHeight: 48
-                }
-            }
-        },
-        MuiTabs: {
-            root: {
-                minHeight: 30,
-                maxHeight: 30
-            }
-        },
-        MuiTab: {
-            root: {
-                minHeight: 30,
-                maxHeight: 30,
-                padding: '6px 0px'
-            }
-        }
         // MuiTableCell: {
         //     root: {  //This can be referred from Material UI API documentation. 
         //         padding: '4px 8px',
